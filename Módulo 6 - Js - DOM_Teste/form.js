326fa28724cca580045e0d554189637c597ed871
Eurípedes simões luz júnior <esimoeslj02@gmail.com>
2024-05-18 23:24:48 -0300

    Trabalhado com formulários
    Primeiro teste de formulário com DOM.

---
+++ Módulo 6 - Js - DOM_Teste/form.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('orderForm')
+
+form.addEventListener('submit', function(ev){
+    const name = document.querySelector('input[name = "name"]').value
+    alert('name')
+
+})
